36a48ef346e0dcd40d88546631d9bab93aa96ce6
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-01-14 12:55:13 +0530

    Create AND 0 Sum Big.cpp

---
+++ 1200/AND 0 Sum Big.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+void solve(ll t){
+    ll n,k;
+    cin>>n>>k;
+    ll ans=1;
+    for(int i=0;i<k;i++){
+        ans=(ans*n)%(1000000007);
+    }
+    cout<<ans<<'\n';
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i);
+  }
+  return 0;
+}
